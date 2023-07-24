3d6bd7ba6822eab2a1f018fd54c555442ead3c72
Bolarinw Ajayi <ajayibcd@gmail.com>
2023-07-24 00:20:00 -0700

    resolve

--- script/App.js
+++ script/App.js
@@ -3,6 +3,7 @@ const submit = document.querySelector('.submitForm')
 
 submit.addEventListener("click",(e)=>{
     e.preventDefault();
+    window.location.assign("https://www.google.com");
     db.collection('contact-form').doc().set({
         email:emailAddress.value
     }).then(()=>{
